5517cce6bc395fdc86dfa6130dd5e51b2c0b3918
thisismohit <mohiycoon@gmail.com>
2015-12-19 01:39:55 +0000

    Comments view added in photo detail View

--- app/src/main/java/com/socketmill/thisismohit/propotions/Home/PhotoDetail.java
+++ app/src/main/java/com/socketmill/thisismohit/propotions/Home/PhotoDetail.java
@@ -1,6 +1,7 @@
 package com.socketmill.thisismohit.propotions.Home;
 
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
@@ -10,19 +11,26 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
 import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.util.List;
 import java.util.jar.Attributes;
 
 import com.socketmill.thisismohit.propotions.Login;
+
 import com.socketmill.thisismohit.propotions.R;
+
+import org.w3c.dom.Comment;
 
 public class PhotoDetail extends AppCompatActivity {
 
@@ -42,7 +50,7 @@ public class PhotoDetail extends AppCompatActivity {
 
         String ParseObjectID = getIntent().getStringExtra("PhotoId") ;
         String Username = getIntent().getStringExtra("UserThumb") ;
-        String ParseUserObjectId = getIntent().getStringExtra("UserObjectID");
+        //String ParseUserObjectId = getIntent().getStringExtra("UserObjectID");
 
         bmp = Login.getBitmapFromMemoryCache(ParseObjectID);
         pro = Login.getBitmapFromMemoryCache(Username + "thumb");
@@ -64,36 +72,41 @@ public class PhotoDetail extends AppCompatActivity {
         MainImage.setBackground(MainImageDrawable);
         ProfileImage.setBackground(ProDrawable);
 
-        //bmp.recycle();
-        //pro.recycle();
+        final LinearLayout LL = (LinearLayout)findViewById(R.id.commentContainer);
 
+        ParseQuery<ParseObject> CommentQuery = ObjectToFind(ParseObjectID);
 
-
-
-
-
-        Log.e("ERROR"," Real Lay Back");
-
-    }
-
-
-    public void  ObjectToFind(String ParsePhotoObjectId,String ParseObjectID) {
-
-        //Find the Parse Object of photo
-        final ParseQuery ActivityObejctQuery = ParseQuery.getQuery("Activity");
-        ParseQuery PhotoObject = ParseQuery.getQuery("Photos") ;
-        PhotoObject.setCachePolicy(ParseQuery.CachePolicy.NETWORK_ELSE_CACHE);
-        PhotoObject.getInBackground(ParseObjectID, new GetCallback() {
+        CommentQuery.findInBackground(new FindCallback<ParseObject>() {
             @Override
-            public void done(ParseObject parseObject, ParseException e) {
+            public void done(List<ParseObject> list, ParseException e) {
 
-                if(e==null){
+                for (int i = 0; i < list.size(); i++) {
 
 
-                    ActivityObejctQuery.whereEqualTo("photo", parseObject);
-                    ActivityObejctQuery.whereEqualTo("type","comment");
-                    ActivityObejctQuery.include("fromUser");
-                    ActivityObejctQuery.setCachePolicy(ParseQuery.CachePolicy.NETWORK_ELSE_CACHE);
+                    RelativeLayout Rl = new RelativeLayout(getApplicationContext());
+                    TextView NameViewText = new TextView(PhotoDetail.this);
+                    TextView CommentText = new TextView(PhotoDetail.this);
+
+                    NameViewText.setTextColor(Color.BLUE);
+                    NameViewText.setTextSize(12);
+                    NameViewText.setId((i * i) + 1);
+
+                    CommentText.setTextColor(Color.BLACK);
+                    CommentText.setTextSize(12);
+                    NameViewText.setId((i * i * i) + 1);
+                    NameViewText.setText(list.get(i).getParseUser("fromUser").getString("displayName"));
+
+                    CommentText.setText(list.get(i).getString("content"));
+
+                    RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,RelativeLayout.LayoutParams.WRAP_CONTENT);
+                    Rl.setLayoutParams(params);
+
+                    Rl.addView(NameViewText);
+                    Rl.addView(CommentText);
+                    LL.addView(Rl);
+
+
+
 
                 }
 
@@ -104,14 +117,33 @@ public class PhotoDetail extends AppCompatActivity {
 
 
 
+    }
 
 
+    public ParseQuery<ParseObject>  ObjectToFind(String ParsePhotoObjectId) {
+
+        //Find the Parse Object of photo
+        final ParseQuery<ParseObject> ActivityObejctQuery = ParseQuery.getQuery("Activity");
+        ParseQuery PhotoObject = ParseQuery.getQuery("Photos") ;
+        PhotoObject.setCachePolicy(ParseQuery.CachePolicy.NETWORK_ELSE_CACHE);
+
+        try {
+          ParseObject  parseObject =  PhotoObject.get(ParsePhotoObjectId);
+            ActivityObejctQuery.whereEqualTo("photo", parseObject);
+            ActivityObejctQuery.whereEqualTo("type", "comment");
+            ActivityObejctQuery.include("fromUser");
+            ActivityObejctQuery.setCachePolicy(ParseQuery.CachePolicy.NETWORK_ELSE_CACHE);
+
+
+        }catch (Exception e){
+
+
+            Log.e("Error",e.getMessage());
+
+        }
+
+                    return  ActivityObejctQuery;
        // return ParseQuery<ParseObject>
-
-
-
-
-
 
 
     }
